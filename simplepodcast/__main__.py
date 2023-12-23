52e262a497d44c889b6d2a13b8967824bd4eac62
Jakob Klepp <jakob-github@truh.in>
2023-12-24 00:00:31 +0100

    start-simplepodcast script

--- simplepodcast/__main__.py
+++ simplepodcast/__main__.py
@@ -1,16 +1,4 @@
-import uvicorn
-
-from .settings import LISTEN_HOST, LISTEN_PORT
-
-
-def main():
-    uvicorn.run(
-        "simplepodcast.simplepodcast:app",
-        host=LISTEN_HOST,
-        port=LISTEN_PORT,
-        reload=True,
-    )
-
+from . import main
 
 if __name__ == "__main__":
     main()
